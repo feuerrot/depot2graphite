54ddb06d5100067689ca82650cee4d2327f927a4
Markus Witt <feuerrot@feuerrot.org>
2019-03-21 21:27:16 +0100

    add actual graphite functionality

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
+import graphitesend
 import logging
 import json
 from fints.client import FinTS3PinTanClient
 
 config = json.load(open("config", "r"))
+
+def get_graphite(config):
+	connection = config["graphite"]
+	graphite = graphitesend.init(
+		prefix=connection["prefix"],
+		system_name="",
+		graphite_server=connection["server"],
+		graphite_port=connection["port"]
+	)
+	return graphite
 
 def get_client(config):
 	login = config["fints"]
@@ -35,10 +46,25 @@ def print_holdings(holdings):
 			holding.name
 		))
 
+def send_holding(graphite, holding):
+	def fmt(h, n):
+		return "{}.{}".format(h.ISIN, n)
+
+	graphite.send(fmt(holding, "pieces"), holding.pieces)
+	graphite.send(fmt(holding, "acquisitionprice"), holding.acquisitionprice)
+	graphite.send(fmt(holding, "total_value"), holding.total_value)
+	graphite.send(fmt(holding, "market_value"), holding.market_value)
+
+def send_holdings(graphite, holdings):
+	for holding in holdings:
+		send_holding(graphite, holding)
+
 def main(config):
 	c = get_client(config)
+	g = get_graphite(config)
 	h = get_holdings(c)
 	print_holdings(h)
+	send_holdings(g, h)
 
 if __name__ == "__main__":
 	main(config)
